3e7cb4c527e8133fa4269e4e9ccd79b68eaae23a
HalimaAbdalla <halima.cfk@gmail.com>
2018-06-02 10:40:30 +0300

    assign.py

---
+++ assign.py
@@ -0,0 +1,13 @@
+listint = [21,22,23,24]
+
+liststrings = ['aviation','engineering','mechanics']
+
+list3 = [1,'mombasa','nairobi',47,2,'kilifi']
+
+tuple1 = (10,30,'june',6,'lamu','jet',11,'khadice')
+
+myset = {'lexus','mazda','subaru','mercedes'}
+
+mydict = {'name':'Halima','age':'19','town':'Lamu'}
+
+print(listint,liststrings,list3,tuple1,myset,mydict)
